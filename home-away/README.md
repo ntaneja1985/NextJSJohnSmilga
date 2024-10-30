4ac6e29493932db10a75f4fbb9b5d4fa8caac053
ntaneja1985 <93190220+ntaneja1985@users.noreply.github.com>
2024-10-31 01:41:12 +0530

    Added the HomeAway Project and its own Readme file
    Added the HomeAway Project and its own Readme file

---
+++ home-away/README.md
@@ -0,0 +1,15 @@
+# Home Away Project using Next.js
+
+1. Create the pages in the app folder
+2. Shadcn UI makes us write less CSS
+3. Every Shadcn UI component has baked in accessibility
+
+ - Create the Next.js app for the project
+ - Set up the pages in app router
+ - Install the shadcn components that are needed
+ - Set up the Navbar and define the various components of Navbar like Logo, NavSearch, DarkMode, LinksDropdown,UserIcon and SignOutLink
+ - Import the shadcn components inside the Navbar including Logo and NavSearch
+ - Install the React icons library
+ - Import the icons inside the Navbar
+ - Copy the CSS for the custom theme we will use and set it up in globals.css
+ - Set up the Navbar in layout.tsx
---
+++ home-away/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "HomeAway",
+  description: "Feel at home away from home",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+      <Navbar/>
+      <main className='container py-10'>{children}</main>
+
+      </body>
+    </html>
+  );
+}
